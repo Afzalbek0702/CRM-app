e7b9b29e6ffff6c71744789e15a9743633db1287
Afzalbek <gofurovafzalbek7@gmail.com>
2026-01-18 15:26:45 +0500

    Update teachers-routes.js

--- routes/teachers-routes.js
+++ routes/teachers-routes.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 import teachersController from "../controller/teachers-controller.js";
 const router = Router();
 
-router.post("/", teachersController.postTeacher);
+router.get("/", teachersController.getAllTeachers);
 router.get("/:id", teachersController.getTeacherById);
+router.post("/", teachersController.postTeacher);
 router.put("/:id", teachersController.updateTeacher);
 router.delete("/:id", teachersController.deleteTeacher);
 export default router;
